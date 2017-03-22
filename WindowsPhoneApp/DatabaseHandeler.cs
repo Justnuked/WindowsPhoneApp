404be01a6e819ee3a1a4702a7b8517e57bbe7475
Justnuked <thommartens14@hotmail.com>
2017-03-22 08:59:40 +0100

    First upload
    Basic functions

---
+++ WindowsPhoneApp/DatabaseHandeler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using WindowsPhoneApp.Models;
+
+namespace WindowsPhoneApp
+{
+    public class DatabaseHandeler
+    {
+        public SQLiteConnection db;
+        public DatabaseHandeler()
+        {
+            db = new SQLiteConnection("Foo");
+            db.CreateTable<Users>();
+        }
+
+        public string Test()
+        {
+            var temp = db.Insert(new Users()
+            {
+                UserName = "Temp",
+                PassWord = "wriuwiuhfwuiefh"
+            });
+
+            return "Temp";
+        }
+
+        public List<Users> QueryVals()
+        {
+            return db.Query<Users>("select * from Users");
+        }   
+    }
+}
---
+++ WindowsPhoneApp/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace WindowsPhoneApp.Models
+{
+   public class Users
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set;}
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        public Users()
+        { 
+        }
+
+
+    }
+}
