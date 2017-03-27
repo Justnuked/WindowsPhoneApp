a9194bb9b402eb422a859413a71fb7a7ac925018
Justnuked <thommartens14@hotmail.com>
2017-03-27 08:40:58 +0200

    Start login functions
    Login functions

--- WindowsPhoneApp/Controllers/Login.cs
+++ WindowsPhoneApp/Controllers/Login.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsPhoneApp.Models;
+using System.Security;
 
 namespace WindowsPhoneApp.Controllers
 {
     public class Login
     {
 
+        DatabaseHandeler dbh = new DatabaseHandeler();
+
         public Login()
         {
  
         }
+
+        //Creates the test accounts
+        public void CreateAccounts()
+        {
+            dbh.db.Insert(new Users(){
+            UserName = "admin",
+            PassWord = "Test"
+        });
+        }
+
+
     }
 }
